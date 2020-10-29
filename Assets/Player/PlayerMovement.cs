9eea911511ea6d99851ab8627805c803ac2feff4
georgealexiou <george@alexiou.xyz>
2020-10-29 02:32:41 +0000

    Added basic movement

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public Rigidbody2D rb;
+
+
+    Vector2 movement;
+
+    public void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+        movement = new Vector2(0f, 0f);
+    }
+}
