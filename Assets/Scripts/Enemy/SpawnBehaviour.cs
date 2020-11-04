22f1cb3907e91191e3ecc029ae3462cb364f44d5
georgealexiou <george@alexiou.xyz>
2020-11-04 06:54:47 +0000

    Started working on enemy spawning scripts

---
+++ Assets/Scripts/Enemy/SpawnBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBehaviour : MonoBehaviour
+{
+
+    public int maxEnemies = 20, currentEnemies = 0, wave = 1;
+    public float spawnRadius = 7f, time = 1.5f;
+    public GameObject spawner;
+    public GameObject[] enemies;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello");
+        SpawnAnEnemy();
+    }
+
+    private IEnumerator SpawnAnEnemy()
+    {
+        Debug.Log("2");
+        Vector2 spawnPos = GameObject.Find("Player").transform.position;
+        Debug.Log("3");
+        spawnPos += Random.insideUnitCircle.normalized * spawnRadius;
+
+        Debug.Log("Enemy Spawning");
+        Instantiate(enemies[Random.Range(0, enemies.Length)], spawnPos, Quaternion.identity);
+        yield return new WaitForSeconds(time);
+        StartCoroutine(SpawnAnEnemy());
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,21 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5;
+
+    public Animator animator;
+    public float moveSpeed = 3f;
     public Rigidbody2D rb;
 
+    private Vector2 movement;
+    
 
-    Vector2 movement;
-
-    public void Update()
+    private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+
+
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
         movement = new Vector2(0f, 0f);
+
     }
 }
