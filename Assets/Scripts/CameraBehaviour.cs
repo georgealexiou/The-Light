8ee8c7a4295716b385b052a14698e6133a332713
Arthur Edelmans <aedelman97@gmail.com>
2020-11-10 18:50:29 +0000

    Adding New Scene

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+
+    public Transform Player;
+    public float dampTime = 0.4f;
+    private Vector3 cameraPos;
+    private Vector3 velocity = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraPos = new Vector3(Player.position.x, Player.position.y, -10f);
+        transform.position = Vector3.SmoothDamp(gameObject.transform.position, cameraPos, ref velocity, dampTime);
+    }
+}
