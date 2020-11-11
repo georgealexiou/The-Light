78403509df039d9935688460bc4572d2182784d3
Arthur Edelmans <aedelman97@gmail.com>
2020-11-11 13:22:38 +0000

    Level1
    Created an Exit prefab, as well as worked on the first level

---
+++ Assets/Scripts/LightBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightBehaviour : MonoBehaviour
+{
+
+    public Light2D light;
+    public GameObject exit;
+    float distance;
+    public Gradient gradient;
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, exit.transform.position);
+
+        Debug.Log(distance);
+        
+        light.color = gradient.Evaluate(distance);
+    }
+}
