4698bf18add8584aa0dd35e6db77b672ea58bad6
georgealexiou <george@alexiou.xyz>
2020-11-11 16:21:10 +0000

    Added Health and Light

--- Assets/Scripts/LightBehaviour.cs
+++ Assets/Scripts/LightBehaviour.cs
@@ -8,16 +8,38 @@ public class LightBehaviour : MonoBehaviour
 
     public Light2D light;
     public GameObject exit;
-    float distance;
+    public GameObject start;
     public Gradient gradient;
+
+    public float timeLimit = 1800;
+    public float intensity = 1.5f, maxOuterRadius = 9f, maxInnerRadius = 1f;
+
+    private float distance;
+    private float totalDistance;
+    private float currentTime;
+
+    void Start()
+    {
+        totalDistance = Vector2.Distance(start.transform.position, exit.transform.position);
+        currentTime = timeLimit;
+    }
 
     // Update is called once per frame
     void Update()
     {
         distance = Vector2.Distance(transform.position, exit.transform.position);
 
-        Debug.Log(distance);
-        
-        light.color = gradient.Evaluate(distance);
+        if (distance > totalDistance)
+            light.color = gradient.Evaluate(1f);
+        else
+            light.color = gradient.Evaluate(distance / totalDistance);
+
+        float newIntensity = currentTime / timeLimit;
+
+        light.intensity = intensity * (currentTime / timeLimit);
+        currentTime -= 1;
+
+        if (light.intensity == 0.2)
+            Debug.Log("YOU LOST");
     }
 }
