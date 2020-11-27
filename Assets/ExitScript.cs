21ebf1da9805f72e8a89e112e6570ce9b2567502
Arthur Edelmans <aedelman97@gmail.com>
2020-11-27 21:17:48 +0000

    1st Build

---
+++ Assets/ExitScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCount - 1)
+        {
+            Debug.Log("Winner, winner, chicken dinner!");
+            SceneManager.LoadScene(0);
+        }
+        Debug.Log(collision.gameObject.name + " : " + gameObject.name + " : Collided");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
