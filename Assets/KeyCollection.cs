a7df5c99c4287d025d7821fdc41a43a19b55745f
Arthur Edelmans <aedelman97@gmail.com>
2020-11-29 17:31:37 +0000

    Fixed rotation issue and added light behaviour as well as implemented key collectable

---
+++ Assets/KeyCollection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCollection : MonoBehaviour
+{
+
+    public GameObject player;
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            player.gameObject.GetComponent<LightBehaviour>().keyCollected = true;
+            gameObject.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
