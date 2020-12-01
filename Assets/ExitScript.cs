f675b3bb1d7a749cefbab84f74281ed7bfee8e22
Arthur Edelmans <aedelman97@gmail.com>
2020-12-01 15:59:33 +0000

    IconAdded and some minor bug fixes

--- Assets/ExitScript.cs
+++ Assets/ExitScript.cs
@@ -5,14 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class ExitScript : MonoBehaviour
 {
+
+    public GameObject player;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCount - 1)
+        if (SceneManager.GetActiveScene().buildIndex == 5)
         {
             Debug.Log("Winner, winner, chicken dinner!");
             SceneManager.LoadScene(0);
         }
         Debug.Log(collision.gameObject.name + " : " + gameObject.name + " : Collided");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        if (player.gameObject.GetComponent<LightBehaviour>().keyCollected == true && player.gameObject.GetComponent<LightBehaviour>().levelHasKey == true){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }else if(player.gameObject.GetComponent<LightBehaviour>().levelHasKey == false) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+            
     }
 }
