2f9452a2742131d9c13d8362bcba8a490faa7fda
Arthur Edelmans <aedelman97@gmail.com>
2020-12-01 16:24:12 +0000

    Revert "Merge remote-tracking branch 'origin/Temp' into Levels"
    This reverts commit 6bac2a0039497c8af8a7e371d91faef861ada310, reversing
    changes made to f675b3bb1d7a749cefbab84f74281ed7bfee8e22.

--- Assets/Scripts/LightBehaviour.cs
+++ Assets/Scripts/LightBehaviour.cs
@@ -9,50 +9,94 @@ public class LightBehaviour : MonoBehaviour
     public Light2D light;
     public GameObject exit;
     public GameObject start;
-    public Gradient gradient;
-    public GameManager gameManager;
+    public GameObject key;
+    public Gradient gradient1;
+    public Gradient gradient2;
 
-    public bool enableColorsShift = true, enableIntensityShift = true;
-    public float timeLimit = 1800f;
+    public bool enableBehaviour = true;
+    public bool levelHasKey = true;
+    public bool keyCollected = false;
+
+    public float timeLimit = 1800;
     public float intensity = 1.5f;
 
     private float distance;
-    private float totalDistance;
+    private float totalDistance2Exit;
+    private float totalDistance2Key;
     private float currentTime;
 
     void Start()
     {
 
-        totalDistance = Vector2.Distance(start.transform.position, exit.transform.position);
+        totalDistance2Exit = Vector2.Distance(start.transform.position, exit.transform.position);
+        if (levelHasKey)
+        {
+            totalDistance2Exit = Vector2.Distance(key.transform.position, exit.transform.position);
+            totalDistance2Key = Vector2.Distance(start.transform.position, key.transform.position);
+        }
         currentTime = timeLimit;
 
-        if (!enableIntensityShift)
+        if (!enableBehaviour && !levelHasKey)
+        {
             light.intensity = intensity;
-
-        if(!enableColorsShift)
-            light.color = gradient.Evaluate(1f);
+            light.color = Color.white;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (enableColorsShift)
-        {
-            distance = Vector2.Distance(transform.position, exit.transform.position);
-
-            if (distance > totalDistance)
-                light.color = gradient.Evaluate(1f);
-            else
-                light.color = gradient.Evaluate(distance / totalDistance);
+        if(currentTime <= 0) {
+            Debug.Log("Game Over");
+            GameObject.FindGameObjectWithTag("Player").SetActive(false);
         }
 
-        if (enableIntensityShift)
+        if (enableBehaviour)
         {
-            light.intensity = intensity * (currentTime / timeLimit);
-            currentTime -= 1;
+            if (levelHasKey)
+            {
+                
+                if (keyCollected)
+                {
+                    distance = Vector2.Distance(transform.position, exit.transform.position);
+                    if (distance > totalDistance2Exit)
+                    {
+                        light.color = gradient2.Evaluate(1f);
+                    }
+                    else
+                    {
+                        light.color = gradient2.Evaluate(distance / totalDistance2Exit);
+                    }
+                    light.intensity = intensity * (currentTime / timeLimit);
+                    currentTime -= 1;
+                }else if (!keyCollected)
+                {
+                    distance = Vector2.Distance(transform.position, key.transform.position);
 
-            if (light.intensity < 0.2f)
-                gameManager.onLevelFailure();
+                    if (distance > totalDistance2Key)
+                        light.color = gradient1.Evaluate(1f);
+                    else
+                        light.color = gradient1.Evaluate(distance / totalDistance2Key);
+
+                    light.intensity = intensity * (currentTime / timeLimit);
+                    currentTime -= 1;
+                }
+            }
+            else
+            {
+                distance = Vector2.Distance(transform.position, exit.transform.position);
+                if (distance > totalDistance2Exit)
+                {
+                    light.color = gradient2.Evaluate(1f);
+                }
+                else
+                {
+                    light.color = gradient2.Evaluate(distance / totalDistance2Exit);
+                }
+                light.intensity = intensity * (currentTime / timeLimit);
+                currentTime -= 1;
+            }
+
         }
 
     }
