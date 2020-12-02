a70f5e3adfbfc0dc36de35840c71518243cb24b7
Arthur Edelmans <aedelman97@gmail.com>
2020-12-02 19:44:38 +0000

    Deleted unneeded sound assets and finished sound system

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,46 +8,66 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
+        Sound();
         SceneManager.LoadScene("FirstLevel");
+
     }
 
     public void TutorialGame()
     {
+        Sound();
         SceneManager.LoadScene("MovementScene2");
     }
 
     public void QuitGame()
     {
+        Sound();
         Application.Quit();
     }
 
     public void restartLevel()
     {
+        Sound();
         Scene scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
     }
 
     public void loadLevel(string name)
     {
+        Sound();
         SceneManager.LoadScene(name);
     }
 
     public void loadMainMenu()
     {
+        Sound();
         SceneManager.LoadScene("TtitleScreenWIP");
     }
 
     public void loadNext()
     {
-        if(SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings - 1)
+        Sound();
+        if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings - 1)
         {
             loadMainMenu();
         }
         else
         {
+            Sound();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
         
+    }
+
+    public void Sound()
+    {
+        FindObjectOfType<SoundManager>().Play("UI");
+
+    }
+
+    public void changeMasterVolume(float vol)
+    {
+        FindObjectOfType<SoundManager>().SetVolume("Music", vol);
     }
 }
  
